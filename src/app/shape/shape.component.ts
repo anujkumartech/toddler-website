679ecb52c7786f14fdbd9eb71f81b3a6f525fc7c
Nicky Donvir <nickydonvir@Nickys-MBP.home>
2024-03-02 16:02:29 -0500

    increase number 40 to 50 in number count

--- src/app/shape/shape.component.ts
+++ src/app/shape/shape.component.ts
@@ -7,7 +7,7 @@ import { Observable, timer } from 'rxjs';
   styleUrls: ['./shape.component.scss']
 })
 export class ShapeComponent {
-  shapes = ['square', 'circle', 'triangle', 'rectangle', 'diamond', 'pentagon'];
+  shapes = ['square', 'circle', 'triangle', 'rectangle', 'diamond', 'pentagon', 'semi-cirlcle','start', 'hexagon'];
 
   currentShapeIndex = 0;
   autoplay = false;
