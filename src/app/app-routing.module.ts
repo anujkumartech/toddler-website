0306f080a74003af8ff441a2853da32cc7b6c84e
Nicky Donvir <nickydonvir@Nickys-MBP.home>
2025-01-16 21:05:41 -0500

    added math page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { NumbersComponent } from './numbers/numbers.component';
 import { AlphabetsComponent } from './alphabets/alphabets.component';
 import { ShapeComponent } from './shape/shape.component';
+import { MathComponent } from './math/math.component';
 
 const routes: Routes = [
   { path: 'numbers', component: NumbersComponent },
   { path: 'alphabets', component: AlphabetsComponent },
   { path: 'shapes', component: ShapeComponent },
+  { path: 'math', component: MathComponent },
   { path: '', redirectTo: '/numbers', pathMatch: 'full' },
 ];
 
